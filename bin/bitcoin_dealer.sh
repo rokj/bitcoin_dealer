618c14410ca1f516394c09b3da5589b0a5efa750
Rok Jaklič <rokj@rasca.net>
2012-05-02 11:59:43 +0200

    Script.

--- bin/bitcoin_dealer.sh
+++ bin/bitcoin_dealer.sh
@@ -5,6 +5,7 @@
 
 SUBJECT="Bitcoin dealer down!"
 EMAIL="your@email.internet"
-if ! ps -aux | grep -v grep | grep "scripts/dealing.py" > /dev/null; then
+if ! ps ax | grep -v grep | grep "scripts/dealing.py" > /dev/null
+then
     echo "bitcoin_dealer is not running?" | mail -s "$SUBJECT" $EMAIL
 fi
